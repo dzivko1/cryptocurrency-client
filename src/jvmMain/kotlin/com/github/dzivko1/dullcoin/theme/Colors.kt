e5b1450c80295e540ca9fca92ded3b1c0f3d2fa5
Kristina Dudjak <dudjakkristina@gmail.com>
2023-05-20 18:58:14 +0200

    Style address book

--- src/jvmMain/kotlin/com/github/dzivko1/dullcoin/theme/Colors.kt
+++ src/jvmMain/kotlin/com/github/dzivko1/dullcoin/theme/Colors.kt
@@ -4,5 +4,7 @@ import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
 val Colors = lightColors(
+    primary = Color(0xFFEF5350),
+    primaryVariant = Color(0xFFB61827),
     background = Color(0xFFCCB9BC)
 )
